2495d5db6e1522a918e55fa3d2b0b2649a1e77b9
Joshua Lynch <jlynch125@hotmail.com>
2022-04-25 14:32:43 -0400

    Added 5.5 Exercises: Objects and Classes, Part 2

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,15 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Test your exercise solutions with print statements here.
+            Student billyTheStudent = new Student("Billy The Student");
+            Student williamTheStudent = new Student("Billy The Student");
+            //Console.WriteLine(billyTheStudent.GetGradeLevel(billyTheStudent.NumberOfCredits));
+            //billyTheStudent.AddGrade(4, 3.0);
+            //billyTheStudent.AddGrade(3, 4.0);
+            //Console.WriteLine(billyTheStudent.Gpa);
+            Console.WriteLine(billyTheStudent.ToString());
+            Console.WriteLine(williamTheStudent.ToString());
+            Console.WriteLine(billyTheStudent.Equals(billyTheStudent));
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -31,20 +31,50 @@ namespace SchoolPractice
         public void AddGrade(int courseCredits, double grade)
         {
             // Update the appropriate properties: NumberOfCredits, Gpa
+            double totalQualityScore = NumberOfCredits * Gpa + grade * courseCredits;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
+
         }
 
         //TODO: Complete the GetGradeLevel method here:
-        public string GetGradeLevel(int credits)
+        public  string GetGradeLevel(int credits)
         {
             // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+
+            //TODO TRY SWITCH
+            if (credits >= 0 && credits <= 29 )
+            {
+                return "Freshman";
+            }else if(credits  <= 59) 
+            {
+                return "Sophomore";
+            }
+            else if (credits <= 89)
+            {
+                return "Junior";
+            }else
+            {
+                return "Senior";
+            }
+
+        }
+
+        // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
+        //  Student objects equal.
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId;
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
+        public override string ToString()
+        {
+            return $"{Name}'s GPA is: {Gpa}  : {GetGradeLevel(NumberOfCredits)}"; 
+        }
 
-        // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
-        //  Student objects equal.
 
     }
 }
